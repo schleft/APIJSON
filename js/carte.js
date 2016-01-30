fc8c8081f9e9d00be3db6ff447bec0d5a5a9edcf
schleft <kevin.legougne@gmail.com>
2016-01-30 09:44:57 +0100

    Carte centré sur nantes

---
+++ js/carte.js
@@ -0,0 +1,12 @@
+var lat = 47.2172500;
+var lon = -1.5533600;
+
+var map = L.map('map').setView([lat, lon], 13);
+
+		L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpandmbXliNDBjZWd2M2x6bDk3c2ZtOTkifQ._QA7i5Mpkd_m30IGElHziw', {
+			maxZoom: 18,
+			attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+				'<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+				'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+			id: 'mapbox.streets'
+		}).addTo(map);
